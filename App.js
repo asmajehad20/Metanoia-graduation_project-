951e9aa6e6b6500d6076e08e1bbcab8e1512d5b1
asmajehad20 <126648311+asmajehad20@users.noreply.github.com>
2023-04-21 18:21:47 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import StartingPage from './screens/StartingPage';
+// import * as friebase from 'firebase';
+import App2 from './Notification';
+
+// import { LogBox } from 'react-native';
+export default function App() {
+  return (
+    <StartingPage />
+    // <App2/>
+    // LogBox.ignoreLogs(['Invalid prop textStyle of type array supplied to Cell']);
+  );
+}
+
+// friebase.initializeApp(firebaseConfig)
