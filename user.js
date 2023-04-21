dbceb10cc1f3e65135b136fabf68787a23079955
asmajehad20 <126648311+asmajehad20@users.noreply.github.com>
2023-04-21 18:20:47 +0300

    Add files via upload

---
+++ user.js
@@ -0,0 +1,16 @@
+
+var userr = "asma";
+var pass = 123;
+var id = 0;
+const userPage = (username, password) => {
+  // userr= "asma";
+  // pass= 123;
+  // id = 0
+
+  // userr = username;
+  // pass = password;
+ 
+}
+
+export default userPage
+export {userr, pass, id}
